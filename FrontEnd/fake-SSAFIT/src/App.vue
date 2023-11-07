52da6a71d37d7cc11f025124b2e28c424e978b96
HJ-dp <skwo777@hufs.ac.kr>
2023-11-08 03:08:09 +0900

    refactor : Change view
    라우터 뷰로 통합

--- FrontEnd/fake-SSAFIT/src/App.vue
+++ FrontEnd/fake-SSAFIT/src/App.vue
@@ -9,30 +9,14 @@
         <link rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
         <theHeader/>
-        <carousel/>
-        <div class="cont p">
-            <cardlist />
-            <br>
-            <partcardlist/>
-        </div>
+        <router-view></router-view>
     </div>
 </template>
 
 <script setup>
-import cardlist from '../src/views/components/cardList.vue';
-import partcardlist from '../src/views/components/partCardList.vue';
 import theHeader from './views/must/header.vue';
-import carousel from './views/must/carousel.vue';
 </script>
 
 <style scoped>
-    .cont {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }
-    .p {
-        margin-top: 1em;
-    }
+
 </style>
