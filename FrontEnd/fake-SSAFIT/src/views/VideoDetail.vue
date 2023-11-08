8a2bb1e3f9fe21d81c8d1c2a8849b228e0b9b243
Hojin <skwo777@naver.com>
2023-11-08 17:55:57 +0900

    feat : Youtube connect

--- FrontEnd/fake-SSAFIT/src/views/VideoDetail.vue
+++ FrontEnd/fake-SSAFIT/src/views/VideoDetail.vue
@@ -1,10 +1,23 @@
 <template>
     <div>
+        <div v-html="getYoutubeUrl()"></div>
         {{ $route.params.id }}
     </div>
 </template>
 
 <script setup>
+import { useRoute } from "vue-router";
+const route = useRoute();
+const isbnParam = route.params.id;
+function getYoutubeUrl() {
+    return `<iframe 
+	width="560" height="315" 
+	src="https://www.youtube.com/embed/${isbnParam}" 
+	title="YouTube video player" frameborder="0" 
+	allow="accelerometer; autoplay; clipboard-write; 
+    encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+</iframe>`;
+}
 
 </script>
 
