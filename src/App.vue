d2db5209f389fc8f687bbd97e30b0513097aadf2
HJ-dp <skwo777@hufs.ac.kr>
2023-11-13 15:41:03 +0900

    refactor

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
         <link rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
         <theHeader/>
-        <router-view></router-view>
+        <router-view/>
     </div>
 </template>
 
